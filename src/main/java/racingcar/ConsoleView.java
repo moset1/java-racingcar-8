e7c0d99aa56ba9d625b3194e7580534cf169fbac
MoseT1 <kimmose981214@gmail.com>
2025-10-27 15:44:50 +0900

    feat (input) : ConsoleView 클래스 입출력 구현
    게임 시작과 결과 출력 책임을 가지는 ConsoleView 클래스 구현
    
    기능으로는
    - 차 이름 입력
    - 시도 횟수 입력
    - 게임의 진행상태 출력
    - 최종 우승자 출력

---
+++ src/main/java/racingcar/ConsoleView.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleView {
+
+    public ConsoleView() {
+    }
+
+    public String readCarNames() {
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    public String readTryCount() {
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
+    }
+
+    public void printRaceStatus(String status) {
+
+        System.out.println(status);
+    }
+
+    public void printWinner(String winner) {
+
+        System.out.println("최종 우승자 : " + winner);
+    }
+}
