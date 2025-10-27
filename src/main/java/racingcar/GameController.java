f3f9eb3433ac08b3448599ae4c195753fddf2312
MoseT1 <kimmose981214@gmail.com>
2025-10-27 17:26:20 +0900

    feat(game): 게임 전체 흐름 제어 로직 구현
    - 사용자 입력, 게임 진행, 결과 출력을 총괄하도록 `GameController`의 `runRacingGame` 메소드를 구현합니다.
    - `RaceService`를 호출하여 각 턴마다 레이스를 실행하고, `ConsoleView`를 통해 결과를 출력합니다.
    - 게임 종료 후 `RaceService`로부터 우승자 정보를 받아와 최종 결과를 출력하도록 흐름을 제어합니다.

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -1,25 +1,54 @@
 package racingcar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
 
     private final ConsoleView consoleView;
     private final InputValidator inputValidator;
+    private final RaceService raceService;
 
     public GameController() {
         this.consoleView = new ConsoleView();
         this.inputValidator = new InputValidator();
+        this.raceService = new RaceService();
+    }
+
+    public void runRacingGame() {
+
+        List<String> carNamesList = getValidCarNames();
+
+        int tryCount = getValidateTryCount();
+
+        List<Player> players = makePlayers(carNamesList);
+
+        playRacingGame(players, tryCount);
+
+        List<Player> winners = raceService.selectWinner(players);
+
+        consoleView.printWinner(winners);
+    }
+
+    private void playRacingGame(List<Player> players, int tryCount) {
+
+        System.out.println();
+        System.out.println("실행 결과");
+
+        for (int i = 0; i < tryCount; i++) {
+            raceService.runRace(players);
+            consoleView.printRaceStatus(players);
+        }
 
     }
 
-    public void run() {
+    private List<Player> makePlayers(List<String> carNamesList) {
 
-        List<String> carNamesList = getValidCarNames();
-        Integer tryCount = getValidateTryCount();
-
-
-
+        List<Player> players = new ArrayList<>();
+        for (String name : carNamesList) {
+            players.add(new Player(name));
+        }
+        return players;
     }
 
     private List<String> getValidCarNames() {
